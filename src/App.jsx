53143cd2c3530ea44d6cf1273297ae3a2fc68498
Pasan Yasara <pasansmart@gmail.com>
2026-01-15 19:25:49 +0530

    managing state and using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import {useState} from 'react';
 
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
-
 function App() {
-  function handleClick(){
-        console.log('Hello World - selected');
-    }
+  let [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
+  function handleClick(selectedButton) {
+    //console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    //console.log(tabContent);
+  }
   return (
     <div>
       <Header />
@@ -21,20 +25,22 @@ function App() {
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleClick}>Components</TabButton>
-            <TabButton onSelect={handleClick}>JSX</TabButton>
-            <TabButton onSelect={handleClick}>Props</TabButton>
-            <TabButton onSelect={handleClick}>State</TabButton>
+            <TabButton onSelect={() => handleClick("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleClick("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
-
+          {selectedTopic}
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,6 +1,8 @@
-export default function TabButton({children,onSelect}){
-
-    return (
-        <li><button onClick={onSelect}>{children}</button></li>
-    );
+export default function TabButton({ children, onSelect }) {
+  console.log("Tab Button Component executing");
+  return (
+    <li>
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
 }
